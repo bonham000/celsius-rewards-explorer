e723dfd814bf8f444c239be5015032111763007f
Sean Smith <sean.smith.2009@gmail.com>
2021-07-07 05:29:14 +0800

    Add comments clarifying rewardsDataMap

--- src/app/rewards.ts
+++ src/app/rewards.ts
@@ -3,9 +3,19 @@ import rewards_02 from "../data/02-rewards.json";
 
 import { CelsiusRewardsDataType } from "./utils";
 
-/**
- * Add more date ranges here for future weekly datasets.
+/** ===========================================================================
+ * Rewards Data
+ * ----------------------------------------------------------------------------
+ * This is where the rewards datasets are imported and we assign them to
+ * their corresponding data range. To add a future dataset:
+ *
+ * 1. Import and process the CSV file.
+ * 2. Import the JSON result here.
+ * 3. Add the corresponding data range.
+ * 4. Add the entry to the rewardsDataMap.
+ * ============================================================================
  */
+
 export type DateRangesType =
   | "June 18, 2021 - June 25, 2021"
   | "June 25, 2021 - July 2, 2021";
@@ -20,13 +30,7 @@ export type RewardsDataMap = Map<DateRangesType, CelsiusRewardsDataType>;
 export const getRewardsDataMap = () => {
   const rewardsDataMap: RewardsDataMap = new Map();
 
-  /**
-   * Initialize map with data. This is where the DATE_IDENTIFIER values
-   * from the csv script get mapped to specific date ranges which the
-   * app can understand.
-   *
-   * Add additional rewards data here in the future when needed.
-   */
+  // Add future datasets here
   rewardsDataMap.set(dateRanges[0], rewards_01);
   rewardsDataMap.set(dateRanges[1], rewards_02);
 
